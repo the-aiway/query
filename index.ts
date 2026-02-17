4a42285776b9a101fda7e2503ccfac0e83d19e64
abelcha <abel.xz@icloud.com>
2026-02-17 17:12:50 +0100

    _

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 // import '../demo/styles/globals.css';
 export { ConnectionPool, type InferredArrowTable } from './duck/ConnectionPool';
-export {
-  DuckQueryWasmProvider,
-  DuckDBContext,
-  useDuckDB,
-  type DuckDBConfig,
-  type DBResource,
-} from './react/DuckDBProvider';
+export { DuckQueryWasmProvider, DuckDBContext, useDuckDB, type DuckDBConfig, type DBResource } from './react/DuckDBProvider';
 import QueryTable from './table/QueryTable';
 export { QueryTable };
-export { DataTable, query, fromJSON, type DataTableSource } from './table/QueryTable';
+export { DataTable } from './table/QueryTable';
 export type { InferSQL, Materialize, InferSQLStrict } from './duck/inferSqlReturntype';
+export { DumpLogger } from './duck/DumpLogger';
 
 export default QueryTable;
